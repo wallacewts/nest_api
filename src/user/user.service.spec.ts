facbaa96d8b95b6b1742df94cabc411b4a76d841
Matheus Wallace <matheuswts2011@gmail.com>
2021-02-21 22:44:09 -0300

    feat(user): add user module

---
+++ src/user/user.service.spec.ts
@@ -0,0 +1,21 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user.module';
+import { UserService } from './user.service';
+
+describe('UserService', () => {
+  let service: UserService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UserService],
+      imports: [TypeOrmModule.forRoot(), UserModule],
+    }).compile();
+
+    service = module.get<UserService>(UserService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
